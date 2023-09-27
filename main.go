f398fa6742efc0616957f2990941903890c275ab
mademeikomons <50975091+mademeikomons@users.noreply.github.com>
2023-09-27 22:06:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -259,7 +259,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	 sus = append(sus, 1033488995)
-	 sus = append(sus, 1120306535)
+	// sus = append(sus, 1120306535)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
